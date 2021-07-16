315157412dafc16ad197ffc9cf5eb6a5f6bf1d05
brayamdesign <45243694+brayamdesign@users.noreply.github.com>
2021-07-16 14:53:05 -0500

    Ajustes Palmex
    Preloader nuevo, bandera idioma, boton ficha técnica.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $(".head").load("../components/head.html");
 $(document).ready(function ($) {
     $("#modalHome").modal();
 });
+
+$(".btn-idioma").on("click", function () {
+    var textBtn = $(".btn-idioma > span").text();
+    if (textBtn == "Change Language") {
+        $(this).html("<img src='images/icons/mexico.svg'><span>Cambiar Idioma</span>");
+    } else {
+        $(this).html("<img src='images/icons/estados-unidos.svg'><span>Change Language</span>");
+    }
+
+});
